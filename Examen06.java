d10d281976f6b15a9a561ecee7f2a9c328d0385f
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-10-17 20:53:36 +0200

    Update Examen06.java

--- Examen06.java
+++ Examen06.java
@@ -1,15 +1,1 @@
 
-package javaapplication79;
-import java.util.Scanner;
-
-public class JavaApplication79 {
-
-
-    public static void main(String[] args) {
-    System.out.println("Introduce tu edad");
-    int edad=new Scanner (System.in).nextInt();
-    int ma18=edad>17 ? 0:1;
-    System.out.println("Si sale 0 eres mayor de edad, si sale 1, no;\n "+ma18);
-    }
-    
-}
