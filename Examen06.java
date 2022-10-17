5d597f278c9ecec912c83770bd44dc17a7806850
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-10-17 20:59:21 +0200

    Update Examen06.java

--- Examen06.java
+++ Examen06.java
@@ -1,1 +1,16 @@
+package examen06;
 
+
+public class Examen06 {
+
+    
+    public static void main(String[] args) {
+    char a=210;
+    char b=3;
+    char c=5;
+    char d=7;
+    char e=9;
+    System.out.println(a+""+b+""+c+""+d+""+e);
+    }
+    
+}
